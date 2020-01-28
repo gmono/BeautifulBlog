5aeeb111277316197b614a4e6be8375c9e4e45b5
月落 <gaozijian0411@foxmail.com>
2020-01-28 19:54:01 +0800

    提出问题：如何解决nodejs执行多个外部命令并等待其结束，同时读取并输出其状态（其标准输出）的问题

---
+++ app/dev.ts
@@ -0,0 +1,19 @@
+/**
+ * 开发服务器 启动根目录的tsconfig.json 编译app目录的程序文件到dist_server
+ *            启动Helper目录的tsconfig.json  编译Helper目录的浏览器帮助模块到 app/Helper/helpers.js 
+ *            启动指定配置文件指定的网站的tsconfig.json 编译其中的ts文件到js
+ *            启动 yarn blog server 实时监控内容变化同步到content目录，同时实时监控配置文件配置的site目录的改动并同步到nowSite
+ * 开发服务器主要用于本项目本身编写时用，若是终端用户请直接使用yarn blog server
+ */
+
+
+
+ import * as cluster from "cluster"
+import { spawn } from "child_process";
+ /**
+  * 启动tsc -w 来监视一个目录的ts文件实时编译
+  * @param dirpath 要监控的目录
+  */
+ async function tscWatch(dirpath:string)
+ {
+ }
