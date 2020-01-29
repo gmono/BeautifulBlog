5f7ec64d1551b7ffdc93442c0846086429af9b28
月落 <gaozijian0411@foxmail.com>
2020-01-29 13:58:13 +0800

    添加create.ts准备添加创建程序 尝试使用child_process 模块控制外部任务 尝试使用cluster模块自动多个自身任务

--- app/dev.ts
+++ app/dev.ts
@@ -9,11 +9,17 @@
 
 
  import * as cluster from "cluster"
-import { spawn } from "child_process";
+import { spawn, exec, execSync } from "child_process";
  /**
   * 启动tsc -w 来监视一个目录的ts文件实时编译
   * @param dirpath 要监控的目录
   */
- async function tscWatch(dirpath:string)
+ function tscWatch(dirpath:string)
  {
+     let child=spawn("tsc -w",{
+         stdio:"pipe"
+     });
+     child.on("message",(msg,send)=>{
+         //接收到输出
+     })
  }
