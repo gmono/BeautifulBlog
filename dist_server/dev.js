180b486b8a212f5143c2f34d5f57ee743d5fc0ec
月落 <gaozijian0411@foxmail.com>
2020-01-30 20:35:37 +0800

    添加等待tsc编译完成的过程

--- dist_server/dev.js
+++ dist_server/dev.js
@@ -12,7 +12,7 @@ const child_process_1 = require("child_process");
  * 启动tsc -w 来监视一个目录的ts文件实时编译
  * @param dirpath 要监控的目录
  */
-function tscWatch(name, dirpath) {
+async function tscWatch(name, dirpath) {
     //需要调查detached在false时，有监听事件时，会不会被自动结束的问题
     let child = child_process_1.exec("tsc -w", {
         cwd: dirpath
@@ -22,23 +22,31 @@ function tscWatch(name, dirpath) {
             console.log(`${name}ERROR:`, stderr);
         }
     });
-    console.log(name, "已启动");
-    //  child.stdout.on("data",(chunk)=>{
-    //      console.log(`${name}输出:\n`,chunk);
-    //  })
-    child.stderr.on("data", (c) => {
-        console.log(`${name}错误:`, c);
+    console.log(`[${name}] `, "已启动,正在等待完成......");
+    return new Promise((resolve) => {
+        //等待输出编译完成后返回
+        child.stdout.on("data", (chunk) => {
+            if (chunk.indexOf("Compilation complete. Watching for file changes") != -1) {
+                //表示已经启动监视
+                console.log(`[${name}] `, "初次编译完成");
+                //实际返回
+                resolve(child);
+            }
+        });
+        child.stderr.on("data", (c) => {
+            console.log(`[${name}] `, `错误:`, c);
+        });
+        child.on("close", (code, signal) => {
+            console.log(`[${name}] `, "已退出", `退出代码${code}`);
+        });
     });
-    child.on("close", (code, signal) => {
-        console.log(name, "已退出", `退出代码${code}`);
-    });
-    return child;
 }
 async function main() {
-    let childs = [
+    console.log("正在启动处理进程......");
+    let childs = await Promise.all([
         tscWatch("App ts监视器", "."),
         tscWatch("Helper ts监视器", "./app/Helper")
-    ];
+    ]);
     //等待所有任务结束 或输入q 结束所有进程
     console.log("输入ctrl+c结束所有监视任务");
 }
