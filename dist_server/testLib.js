8eb2b116e9250015f091bcef665d85efa6f05dbe
月落 <gaozijian0411@foxmail.com>
2020-02-16 21:20:14 +0800

    添加testLib为调试入口

--- dist_server/testLib.js
+++ dist_server/testLib.js
@@ -2,16 +2,4 @@
 /**
  * 此程序用于各种库和方法的测试，不算
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const ora = require("ora");
-let sp = ora("loading").start();
-const delay = require("delay");
-let now = 0;
-async function change() {
-    now++;
-    sp.text = "hello" + now;
-    sp.color = "yellow";
-    await delay(500);
-    change();
-}
-change();
+console.log("hello ")
