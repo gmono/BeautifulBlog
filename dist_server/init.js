3942f01a6e3c9ff6a69b0d97e2b4de0ef9252ffd
月落 <gaozijian0411@foxmail.com>
2020-02-19 15:07:06 +0800

    innerCopy函数正在编写中（可复制snapshot路径）

--- dist_server/init.js
+++ dist_server/init.js
@@ -9,6 +9,49 @@ const execa = require("execa");
  * 如果目录不存在可自动创建目录（可选）
  */
 const fse = require("fs-extra");
+const path = require("path");
+const walk = require("walk");
+//工具函数区域
+/**
+ * 复制，通过write(read(file)) 功能继承自fse.copy
+ * @param src 源地址
+ * @param dest 目的地址
+ */
+async function innerCopy(src, dest) {
+    let info = await fse.lstat(src);
+    let copyFile = async (s, d) => {
+        let rd = fse.createReadStream(s);
+        let wd = fse.createWriteStream(d);
+        return new Promise((r) => {
+            wd.addListener("finish", () => r());
+            rd.pipe(wd);
+        });
+    };
+    if (info.isFile()) {
+        await copyFile(src, dest);
+    }
+    else {
+        //目录
+        await fse.ensureDir(dest);
+        //开始复制目录 遍历src目录树
+        let mon = walk.walk(src);
+        mon.on("file", async (dirpath, filename, next) => {
+            //把文件复制到对应位置 把dirpath和filename合成完整src路径 从中生成dest路径 复制文件
+            let allpath = path.resolve(dirpath, filename.name);
+            //在allpath中去除src的部分
+            let endpath = allpath.slice(src.length);
+            let destpath = path.resolve(dest, endpath);
+            let destdir = path.parse(destpath).dir;
+            await fse.ensureDir(destdir);
+            //复制文件
+            await copyFile(allpath, destpath);
+            next();
+        });
+        return new Promise((resolve) => {
+            mon.on("end", () => resolve());
+        });
+    }
+}
 /**
  * 在目录中创建博客
  * @param dirpath 要创建博客的目录
@@ -31,9 +74,9 @@ async function createBlog(dirpath, autocreate = true) {
     ]);
     console.log("目录创建完毕");
     await Promise.all([
-        fse.copy(`${__dirname}/../sites/default`, `${dirpath}/sites/default`),
-        fse.copy(`${__dirname}/../config/default.json`, `${dirpath}/config/default.json`),
-        fse.copy(`${__dirname}/../assets`, `${dirpath}/assets`)
+        innerCopy(`${__dirname}/../sites/default`, `${dirpath}/sites/default`),
+        innerCopy(`${__dirname}/../config/default.json`, `${dirpath}/config/default.json`),
+        innerCopy(`${__dirname}/../assets`, `${dirpath}/assets`)
     ]);
     console.log("文件复制完毕");
     console.log("开始创建git仓库");
