00cf626800e020d0af6296bde146c89c7cc2b00a
月落 <gaozijian0411@foxmail.com>
2020-02-28 12:43:39 +0800

    修改manager

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,7 @@
 import * as fse from 'fs-extra';
 import { IConfig } from '../Interface/IConfig';
 import path = require("path");
+import * as walk from "walk"
 
 export function readConfig(name:string){
     return fse.readJson(path.resolve(__dirname,`../../config/${name}.json`)) as Promise<IConfig>;
@@ -11,4 +12,55 @@ export async function runInDir(dirpath:string,func:Function){
     process.chdir(dirpath);
     await func();
     process.chdir(s);
+}
+//工具函数区域
+/**
+ * 复制，通过write(read(file)) 功能继承自fse.copy
+ * @param src 源地址
+ * @param dest 目的地址
+ */
+export async function innerCopy(src: string, dest: string) {
+    let info = await fse.lstat(src);
+    let copyFile = async (s: string, d: string) => {
+        let rd = fse.createReadStream(s);
+        let wd = fse.createWriteStream(d);
+        return new Promise<void>((r) => {
+            wd.addListener("finish", () => {
+                // console.log(`复制文件 ${s} 到 ${d}`)
+                r();
+            });
+            rd.pipe(wd);
+        });
+    };
+    if (info.isFile()) {
+        await copyFile(src, dest);
+    }
+    else {
+        //目录
+        await fse.ensureDir(dest);
+        //开始复制目录 遍历src目录树
+        let mon = walk.walk(src);
+        mon.on("directory", (base, name, next) => {
+            // console.log(path.resolve(base,name.name));
+            next();
+        });
+        mon.on("file", async (dirpath, filename, next) => {
+            //把文件复制到对应位置 把dirpath和filename合成完整src路径 从中生成dest路径 复制文件
+            //不带src路径前缀的目录路径 从1开始截取是为了去掉path前面的/ 以免被认为是从root开始
+            let endDirpath = dirpath.slice(src.length).slice(1);
+            let allpath = path.resolve(dirpath, filename.name); //源地址
+            // console.log(allpath);
+            //在allpath中去除src的部分
+            let destdir = path.resolve(dest, endDirpath); //目的目录地址
+            let destpath = path.resolve(destdir, filename.name); //目的文件地址
+            console.log(dest, endDirpath, destdir);
+            await fse.ensureDir(destdir);
+            //复制文件
+            await copyFile(allpath, destpath);
+            next();
+        });
+        return new Promise<void>((resolve) => {
+            mon.on("end", () => resolve());
+        });
+    }
 }
--- app/manager.ts
+++ app/manager.ts
@@ -23,7 +23,7 @@ import generate from './generator';
 import changesite from './changesite';
 import * as fse from 'fs-extra';
 import * as path from 'path';
-import { readConfig,runInDir } from './lib/utils';
+import { readConfig,runInDir, innerCopy } from './lib/utils';
 export async function getNames(){
     //执行git remote
     //all表示要手机process的所有输出
@@ -68,6 +68,9 @@ export async function listRemote(){
  * 用于初始化一个blog的git相关内容
  * 包括 创建仓库 初次提交和push（包括设置trach branch)  创建.gitignore 
  * 此函数执行后blog才可以进行后续manage操作
+ * 仅对独立blog目录使用
+ * 对于主仓库（即直接clone下来的包含blog和代码的仓库）请勿使用此函数（可能会破坏原有仓库结构）
+ * @param dirpath 独立blog目录的路径
  */
 export async function initGit(dirpath:string){
     console.log("开始创建git仓库");
@@ -77,20 +80,17 @@ export async function initGit(dirpath:string){
         await execa("git init",{
             stdio:"inherit",
         });
-        ////此处需要添加复制.gitignore的操作
+        //复制gitginore模板到blog目录的.gitignore文件
+        await innerCopy(path.resolve(__dirname,"../static/template_gitignore.txt"),"./.gitignore");
         await execa("git add .",{stdio:"inherit"});
         await execa(`git commit -m "创建博客" `,{stdio:"inherit"});
-        ////此处需要添加初次提交操作
     });
     console.log("创建完毕")
 }
-/**
- * 提交到某个仓库   
- * @param name 仓库名
- */
-export async function pushToRemote(name?:string){
-    let push=async (name)=>{
-        //提交到某个仓库 generate changesite add commit push 一条龙
+
+export async function pushToRepos(name:string){
+    //读取配置文件保存的用户名和密码并提交
+    //提交到某个仓库 generate changesite add commit push 一条龙
         //自动读取名称相同的配置文件
         await generate(name)
         const config=await readConfig(name);
@@ -101,18 +101,39 @@ export async function pushToRemote(name?:string){
         await execa(`git commit -m "${await getChangeInfoText()}"`);
         //此处只提供提交到master分支 假设已经设置了trach branch
         await execa(`git push ${name} master`)
-
-    }
+}
+/**
+ * 提交到某个仓库   
+ * @param name 仓库名
+ */
+export async function pushToRemote(name?:string){
     if(name!=null){
         //提交到单个仓库
-        await push((await getRemote(name)).url);
+        await pushToRepos((await getRemote(name)).url);
         console.log(`成功提交到:${name}`)
     }
     else{
         //提交到所有仓库
-        (await getRemoteList()).map(v=>({name:v.name,res:push(v.url)})).forEach(async v=>{
+        (await getRemoteList()).map(v=>({name:v.name,res:pushToRepos(v.url)})).forEach(async v=>{
             await v.res;
             console.log(`成功提交到:${v.name}`);
         });
     }
+}
+
+/**
+ * 代理git remote add的功能，增加自动进行初次提交(假设远端为空仓库) 自动保存提交用用户名密码的功能
+ * 确保后续工作进行并确保远端仓库与本地一致（如果远端仓库与本地冲突则自动解决冲突）
+ * @param reposname 仓库名
+ * @param giturl remote url
+ */
+export async function addRepos(reposname:string,giturl:URL){
+    //有待完善
+}
+/**
+ * 代理移除仓库的功能
+ * @param reposname 仓库名
+ */
+export async function removeRepos(reposname:string){
+    await execa(`git remove ${reposname}`);
 }
