22ec6b292b9dd2f8d73b3b63e0a3814e26dade1b
月落 <gaozijian0411@foxmail.com>
2020-02-29 18:31:50 +0800

    基本完成txt文件的transformer（尚未测试)

--- dist_server/transform.js
+++ dist_server/transform.js
@@ -47,13 +47,45 @@ async function transform(filepath, configname = "default", ...args) {
     const func = transformTable[ext];
     return func(filepath, config, globalconfig, ...args);
 }
+const yaml = require("yaml");
 async function transformTXT(filepath, config, globalconfig, ...args) {
-    //转换txt文件到html
+    //转换txt文件到html txt如果没有yaml的元数据则把第一行当作标题其余元数据为null
+    //txt文件的meta由同名yaml提供
     let txt = (await fse.readFile(filepath)).toString();
+    //读取元数据
+    let read = async (filepath) => {
+        //读取元数据(从yaml 与frontmatter可兼容)
+        let info = path.parse(filepath);
+        let name = path.resolve(info.dir, info.name);
+        let metaname = `${name}.yaml`;
+        //如果不存在则把txt的第一行做标题 第二行当作时间(yyyy-MM-dd hh:mm:ss) 第三行往下当作正文
+        //如果存在则把txt整个当作正文
+        let meta = {};
+        if (!(await fse.pathExists(metaname))) {
+            meta.title = txt.split("\n")[0];
+            meta.date = new Date(txt.split("\n")[1]);
+            txt = txt.split("\n").slice(2).join("\n");
+        }
+        else {
+            meta = yaml.parse(metaname);
+        }
+        //返回元数据与正文
+        return {
+            meta: meta,
+            content: txt
+        };
+    };
+    //读取
+    let obj = await read(filepath);
     let html = template(path.resolve(__dirname, "../static/txt_template.html"), {
-        content: txt
+        content: obj.content
     });
     //返回
+    return {
+        html: html,
+        raw: Buffer.from(txt),
+        meta: obj.meta
+    };
 }
 let first = true;
 let baseurl = "/";
