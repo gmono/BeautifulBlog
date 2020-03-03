7a112b8f594c60e00f17fdd93176cd0f921e02d6
月落 <gaozijian0411@foxmail.com>
2020-03-03 15:21:50 +0800

    决定采用prompts库，完善mamage程序

--- app/manager.ts
+++ app/manager.ts
@@ -24,6 +24,10 @@ import changesite from './changesite';
 import * as fse from 'fs-extra';
 import * as path from 'path';
 import { readConfig,runInDir, innerCopy } from './lib/utils';
+
+///基本功能函数部分
+
+///获取信息系列 包括获取仓库url和名字等
 export async function getNames(){
     //执行git remote
     //all表示要手机process的所有输出
@@ -45,7 +49,9 @@ export async function getRemoteList():Promise<RemoteItem[]>{
     return Promise.all(res);
 }
 
-
+/**
+ * 用于生成提交说明
+ */
 async function getChangeInfoText(){
     //获取更改信息文本 主要用于合成提交消息
     //这里需要generator和watch等程序做log操作 因此这里展示只返回固定的消息
@@ -53,17 +59,9 @@ async function getChangeInfoText(){
 }
 
 
-/**
- * 列出所有仓库
- */
-export async function listRemote(){
-    console.log("发布地址列表:");
-    (await getRemoteList()).forEach(v=>{
-        console.log(`Name:${v.name} URL:${v.url}`);
-    })
-}
 
 
+///操作函数部分
 /**
  * 用于初始化一个blog的git相关内容
  * 包括 创建仓库 初次提交和push（包括设置trach branch)  创建.gitignore 
@@ -88,6 +86,10 @@ export async function initGit(dirpath:string){
     console.log("创建完毕")
 }
 
+/**
+ * 提交到某个仓库，假定不是初次提交（初次提交可能需要输入用户名密码和设置跟踪分支）
+ * @param name 要提交到的仓库
+ */
 export async function pushToRepos(name:string){
     //读取配置文件保存的用户名和密码并提交
     //提交到某个仓库 generate changesite add commit push 一条龙
@@ -102,9 +104,47 @@ export async function pushToRepos(name:string){
         //此处只提供提交到master分支 假设已经设置了trach branch
         await execa(`git push ${name} master`)
 }
+
+
 /**
- * 提交到某个仓库   
- * @param name 仓库名
+ * 只能在initGit之后调用
+ * 代理git remote add的功能，增加自动进行初次提交(假设远端为空仓库) 自动保存提交用用户名密码的功能
+ * 确保后续工作进行并确保远端仓库与本地一致（如果远端仓库与本地冲突则自动解决冲突）
+ * @param reposname 仓库名
+ * @param giturl remote url
+ */
+export async function addRepos(reposname:string,giturl:URL){
+    //指定git指令添加仓库，并进行初次提交
+    //目前的冲突解决策略是直接放弃显示失败，因此务必保持remote仓库为空
+    await execa(`git remote add ${reposname} ${giturl}`)
+    //初次提交 目前只支持master分支不支持自定义分支指定
+    await execa(`git push -u ${reposname} master`)
+}
+/**
+ * 代理移除仓库的功能
+ * @param reposname 仓库名
+ */
+export async function removeRepos(reposname:string){
+    await execa(`git remove ${reposname}`);
+}
+
+
+
+///用户接口部分
+
+/**
+ * 用户接口，列出所有仓库
+ */
+export async function listRemote(){
+    console.log("发布地址列表:");
+    (await getRemoteList()).forEach(v=>{
+        console.log(`Name:${v.name} URL:${v.url}`);
+    })
+
+    
+/**
+ * 用户接口，提交到某个仓库   
+ * @param name 仓库名，如果不提供则提交到所有仓库
  */
 export async function pushToRemote(name?:string){
     if(name!=null){
@@ -121,19 +161,11 @@ export async function pushToRemote(name?:string){
     }
 }
 
+import * as prompt from "prompts"
 /**
- * 代理git remote add的功能，增加自动进行初次提交(假设远端为空仓库) 自动保存提交用用户名密码的功能
- * 确保后续工作进行并确保远端仓库与本地一致（如果远端仓库与本地冲突则自动解决冲突）
- * @param reposname 仓库名
- * @param giturl remote url
+ * 用户接口 添加仓库 提示输入名字和url
  */
-export async function addRepos(reposname:string,giturl:URL){
-    //有待完善
-}
-/**
- * 代理移除仓库的功能
- * @param reposname 仓库名
- */
-export async function removeRepos(reposname:string){
-    await execa(`git remove ${reposname}`);
+export async function add()
+{
+    
 }
--- dist_server/manager.js
+++ dist_server/manager.js
@@ -5,6 +5,8 @@ const generator_1 = require("./generator");
 const changesite_1 = require("./changesite");
 const path = require("path");
 const utils_1 = require("./lib/utils");
+///基本功能函数部分
+///获取信息系列 包括获取仓库url和名字等
 async function getNames() {
     //执行git remote
     //all表示要手机process的所有输出
@@ -28,21 +30,15 @@ async function getRemoteList() {
     return Promise.all(res);
 }
 exports.getRemoteList = getRemoteList;
+/**
+ * 用于生成提交说明
+ */
 async function getChangeInfoText() {
     //获取更改信息文本 主要用于合成提交消息
     //这里需要generator和watch等程序做log操作 因此这里展示只返回固定的消息
     return "更新文章";
 }
-/**
- * 列出所有仓库
- */
-async function listRemote() {
-    console.log("发布地址列表:");
-    (await getRemoteList()).forEach(v => {
-        console.log(`Name:${v.name} URL:${v.url}`);
-    });
-}
-exports.listRemote = listRemote;
+///操作函数部分
 /**
  * 用于初始化一个blog的git相关内容
  * 包括 创建仓库 初次提交和push（包括设置trach branch)  创建.gitignore
@@ -67,6 +63,10 @@ async function initGit(dirpath) {
     console.log("创建完毕");
 }
 exports.initGit = initGit;
+/**
+ * 提交到某个仓库，假定不是初次提交（初次提交可能需要输入用户名密码和设置跟踪分支）
+ * @param name 要提交到的仓库
+ */
 async function pushToRepos(name) {
     //读取配置文件保存的用户名和密码并提交
     //提交到某个仓库 generate changesite add commit push 一条龙
@@ -83,32 +83,18 @@ async function pushToRepos(name) {
 }
 exports.pushToRepos = pushToRepos;
 /**
- * 提交到某个仓库
- * @param name 仓库名
- */
-async function pushToRemote(name) {
-    if (name != null) {
-        //提交到单个仓库
-        await pushToRepos((await getRemote(name)).url);
-        console.log(`成功提交到:${name}`);
-    }
-    else {
-        //提交到所有仓库
-        (await getRemoteList()).map(v => ({ name: v.name, res: pushToRepos(v.url) })).forEach(async (v) => {
-            await v.res;
-            console.log(`成功提交到:${v.name}`);
-        });
-    }
-}
-exports.pushToRemote = pushToRemote;
-/**
+ * 只能在initGit之后调用
  * 代理git remote add的功能，增加自动进行初次提交(假设远端为空仓库) 自动保存提交用用户名密码的功能
  * 确保后续工作进行并确保远端仓库与本地一致（如果远端仓库与本地冲突则自动解决冲突）
  * @param reposname 仓库名
  * @param giturl remote url
  */
 async function addRepos(reposname, giturl) {
-    //有待完善
+    //指定git指令添加仓库，并进行初次提交
+    //目前的冲突解决策略是直接放弃显示失败，因此务必保持remote仓库为空
+    await execa(`git remote add ${reposname} ${giturl}`);
+    //初次提交 目前只支持master分支不支持自定义分支指定
+    await execa(`git push -u ${reposname} master`);
 }
 exports.addRepos = addRepos;
 /**
@@ -119,4 +105,39 @@ async function removeRepos(reposname) {
     await execa(`git remove ${reposname}`);
 }
 exports.removeRepos = removeRepos;
+///用户接口部分
+/**
+ * 用户接口，列出所有仓库
+ */
+async function listRemote() {
+    console.log("发布地址列表:");
+    (await getRemoteList()).forEach(v => {
+        console.log(`Name:${v.name} URL:${v.url}`);
+    });
+    /**
+     * 用户接口，提交到某个仓库
+     * @param name 仓库名，如果不提供则提交到所有仓库
+     */
+    export async function pushToRemote(name) {
+        if (name != null) {
+            //提交到单个仓库
+            await pushToRepos((await getRemote(name)).url);
+            console.log(`成功提交到:${name}`);
+        }
+        else {
+            //提交到所有仓库
+            (await getRemoteList()).map(v => ({ name: v.name, res: pushToRepos(v.url) })).forEach(async (v) => {
+                await v.res;
+                console.log(`成功提交到:${v.name}`);
+            });
+        }
+    }
+    import * as prompt from "prompts";
+    /**
+     * 用户接口 添加仓库 提示输入名字和url
+     */
+    export async function add() {
+    }
+}
+exports.listRemote = listRemote;
 //# sourceMappingURL=manager.js.map
